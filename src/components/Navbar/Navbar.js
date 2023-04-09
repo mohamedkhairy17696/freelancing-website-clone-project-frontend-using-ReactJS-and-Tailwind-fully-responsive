8ea1da4bb38b70984f3a668d8e3e43414ac52536
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-04-10 00:02:35 +0200

    handling bug in mobile Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -188,6 +188,14 @@ const Navbar = () => {
                   {item.name}
                 </Disclosure.Button>
               ))}
+              <Disclosure.Button
+                as="a"
+                href="/"
+                className="hover:bg-gray-700 text-white text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                aria-current="page"
+              >
+                Become A Seller
+              </Disclosure.Button>
             </div>
           </Disclosure.Panel>
         </>
