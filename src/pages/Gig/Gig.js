eac9886e18ae3a84d78cdebf58eee4ecc36283b1
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-04-09 22:26:32 +0200

    commit

--- src/pages/Gig/Gig.js
+++ src/pages/Gig/Gig.js
@@ -143,7 +143,7 @@ const Gig = () => {
 
               <button
                 type="button"
-                className="w-96 mt-8	text-center focus:outline-none text-white bg-emerald-600 hover:bg-emerald-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                className="w-96 mt-8	text-center focus:outline-none text-white bg-emerald-600 hover:bg-emerald-700 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
               >
                 Continue
               </button>
