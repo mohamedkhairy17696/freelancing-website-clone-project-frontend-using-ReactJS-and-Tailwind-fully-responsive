83ea279eec18652e3a385c8da0911c7e31b9b602
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-04-09 02:44:10 +0200

    gig details page

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
       children: [
         {
           path: "/",
-          element: <Home />,
+          element: <Gig />,
         },
         {
           path: "/gigs",
---
+++ src/pages/Gig/Gig.js
@@ -0,0 +1,271 @@
+import React from "react";
+import { Slider } from "infinite-react-carousel/lib";
+const Gig = () => {
+  return (
+    <div className="gig">
+      <div className="container mx-6 my-6 lg:mx-20 lg:my-20">
+        <p className="text-md font-semibold mb-5">
+          Frellancer / Graphics & Design
+        </p>
+        <h1 className="text-2xlg font-bold mb-2">
+          I will do business modern minimalist logo design
+        </h1>
+        <div className="flex mt-5">
+          <img
+            className="w-12 h-12 rounded-full"
+            src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt="Rounded avatar"
+          />
+          <p className="mt-2.5 mx-3">Emma</p>
+          <div class="flex items-center">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-yellow-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>First star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-yellow-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Second star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-yellow-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Third star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-yellow-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Fourth star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-300 dark:text-gray-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Fifth star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+          </div>
+        </div>
+
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-1 lg:grid-cols-2 justify-center	items-center">
+          <div className="mt-12 mr-12">
+            <Slider slidesToShow={1} arrowsScroll={1} className="slider">
+              <img
+                className="rounded"
+                src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <img
+                className="rounded"
+                src="https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <img
+                className="rounded"
+                src="https://images.pexels.com/photos/1054777/pexels-photo-1054777.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </Slider>
+            <h2 className="text-xl font-bold mt-6">About This Gig</h2>
+            <p className="mt-3">
+              Welcome to my creative minimalist logo design gig.
+              <br /> This gig is the league of extremely talented logo
+              designers.
+              <br /> A custom logo design helps in making your brand stand out
+              from the competition.
+              <br /> It establishes an image in the minds of your customer.
+              <br /> We will create extra-ordinary designs as per your
+              requirements.
+            </p>
+          </div>
+          <div className=" items-start content-start">
+            <div class="max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="flex justify-between">
+                <h3>1 Logo Design</h3>
+                <h2 className="text-slate-500">$ 70</h2>
+              </div>
+              <p className="mt-8 text-slate-500">
+                I will create a unique high quality Logo Design based on a
+                description that you give me
+              </p>
+              <div className="flex justify-between my-8">
+                <div className="flex">
+                  <img className="w-4 mr-2" src="/img/clock.png" alt="" />
+                  <span className="mb-1">2 Days Delivery</span>
+                </div>
+                <div className="flex">
+                  <img className="w-4 mr-2" src="/img/recycle.png" alt="" />
+                  <span className="mb-1">3 Revisions</span>
+                </div>
+              </div>
+              <div className="features">
+                <div className="flex mb-1">
+                  <img className="w-4 mr-2" src="/img/greencheck.png" alt="" />
+                  <span>Prompt writing</span>
+                </div>
+                <div className="flex mb-1">
+                  <img className="w-4 mr-2" src="/img/greencheck.png" alt="" />
+                  <span>Artwork delivery</span>
+                </div>
+                <div className="flex mb-1">
+                  <img className="w-4 mr-2" src="/img/greencheck.png" alt="" />
+                  <span>Image upscaling</span>
+                </div>
+                <div className="flex mb-1">
+                  <img className="w-4 mr-2" src="/img/greencheck.png" alt="" />
+                  <span>Additional design</span>
+                </div>
+              </div>
+
+              <button
+                type="button"
+                className="w-96 mt-8	text-center focus:outline-none text-white bg-emerald-600 hover:bg-emerald-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              >
+                Continue
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="mt-12">
+          <h2 className="text-xl font-bold">About The Seller</h2>
+          <div className="flex mt-5">
+            <img
+              className="w-20 h-20 rounded-full"
+              src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt="Rounded avatar"
+            />
+            <p className="mt-7 mx-3">Emma</p>
+            <div class="flex items-center">
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5 text-yellow-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>First star</title>
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5 text-yellow-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Second star</title>
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5 text-yellow-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Third star</title>
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5 text-yellow-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Fourth star</title>
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5 text-gray-300 dark:text-gray-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Fifth star</title>
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+              </svg>
+            </div>
+          </div>
+          <button class="ml-24 bg-transparent hover:bg-emerald-600	 text-emerald-600 font-semibold hover:text-white py-2 px-4 border border-emerald-600 hover:border-transparent rounded">
+            Contact Me
+          </button>
+        </div>
+
+        <div class="mt-8 max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="">
+            <div className="flex justify-between">
+              <div>
+                <div className="my-3">
+                  <p>From</p>
+                  <h3 className="mt-1 text-xl font-semibold">USA</h3>
+                </div>
+                <div className="my-3">
+                  <p>Avg. response time</p>
+                  <h3 className="mt-1 text-xl font-semibold">4 Hours</h3>
+                </div>
+                <div className="my-3">
+                  <p>Languages</p>
+                  <h3 className="mt-1 text-xl font-semibold">English</h3>
+                </div>
+              </div>
+              <div>
+                <div className="my-3">
+                  <p>Member since</p>
+                  <h3 className="mt-1 text-xl font-semibold">USA</h3>
+                </div>
+                <div className="my-3">
+                  <p>Avg. response time</p>
+                  <h3 className="mt-1 text-xl font-semibold">4 Hours</h3>
+                </div>
+              </div>
+            </div>
+            <hr className="my-6" />
+            <p>
+              My name is Anna, I enjoy creating AI generated art in my spare
+              time. I have a lot of experience using the AI program and that
+              means I know what to prompt the AI with to get a great and
+              incredibly detailed result.
+            </p>
+          </div>
+        </div>
+
+        <div className="reviews">
+          <h2 className="text-xl font-bold mt-20">Reviews</h2>
+          <div className=""></div>
+          <hr />
+          <div className=""></div>
+          <hr />
+          <div className=""></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gig;
