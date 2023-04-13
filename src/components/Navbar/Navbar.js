aeb40a0b4c579aef7df6bb3e7686a09521efe05a
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-04-13 12:22:04 +0200

    commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -98,32 +98,36 @@ const Navbar = () => {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="/mygigs"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Gigs
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            to="/add"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Add New Gig
-                          </Link>
-                        )}
-                      </Menu.Item>
+                      {currentUser.isSeller && (
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to="/mygigs"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Gigs
+                            </Link>
+                          )}
+                        </Menu.Item>
+                      )}
+                      {currentUser.isSeller && (
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              to="/add"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Add New Gig
+                            </Link>
+                          )}
+                        </Menu.Item>
+                      )}
                       <Menu.Item>
                         {({ active }) => (
                           <Link
