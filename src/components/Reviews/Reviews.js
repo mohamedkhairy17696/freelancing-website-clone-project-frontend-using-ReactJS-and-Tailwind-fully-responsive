7083d28ef3b7ea36cdd520b296e12851a11d7b75
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-04-17 17:39:13 +0200

    commit

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -4,6 +4,7 @@ import newRequest from "../../utils/newRequest";
 import Spinner from "../Spinner/Spinner";
 
 const Reviews = ({ gigId }) => {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
   const [data, setData] = useState([]);
   const [desc, setDesc] = useState("");
   const [star, setStar] = useState("");
@@ -58,40 +59,42 @@ const Reviews = ({ gigId }) => {
             <hr className="my-6" />{" "}
           </div>
         ))}
-      <form
-        className="sm:w-10/12 md:w-4/5 lg:w-3/5 mr-16"
-        onSubmit={(e) => handleSubmit(e)}
-      >
-        <div className="mb-6">
-          <textarea
-            id="message"
-            value={desc}
-            onChange={(e) => setDesc(e.target.value)}
-            rows="4"
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Write Your opinion"
-          ></textarea>
-        </div>
-        <select
-          value={star}
-          onChange={(e) => setStar(e.target.value)}
-          id=""
-          className="w-1/5 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-emerald-600 focus:border-emerald-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-600 dark:focus:border-emerald-600"
+      {!currentUser?.isSeller && (
+        <form
+          className="sm:w-10/12 md:w-4/5 lg:w-3/5 mr-16"
+          onSubmit={(e) => handleSubmit(e)}
         >
-          <option selected>Choose N.of stars</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
-        </select>
-        <button
-          type="submit"
-          class="text-white text-white bg-emerald-600 hover:bg-emerald-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-        >
-          Send
-        </button>
-      </form>
+          <div className="mb-6">
+            <textarea
+              id="message"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+              rows="4"
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Write Your opinion"
+            ></textarea>
+          </div>
+          <select
+            value={star}
+            onChange={(e) => setStar(e.target.value)}
+            id=""
+            className="w-1/5 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-emerald-600 focus:border-emerald-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-600 dark:focus:border-emerald-600"
+          >
+            <option selected>Choose N.of stars</option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+          <button
+            type="submit"
+            class="text-white text-white bg-emerald-600 hover:bg-emerald-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            Send
+          </button>
+        </form>
+      )}
     </div>
   );
 };
