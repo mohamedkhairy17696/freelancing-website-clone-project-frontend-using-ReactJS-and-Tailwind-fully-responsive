631b4ee0ba9963ed4fdecd9fbe35c987c3763813
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-05-04 14:20:33 +0300

    commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -100,7 +100,7 @@ const Navbar = () => {
                         i18n.changeLanguage("en");
                       }}
                       type="button"
-                      className="mt-2 text-center focus:outline-none text-white bg-emerald-600 hover:bg-emerald-700 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                      className="mt-2 text-center focus:outline-none text-white bg-emerald-600 hover:bg-emerald-700 font-medium rounded-lg text-sm px-2 py-1 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                     >
                       EN
                     </button>
